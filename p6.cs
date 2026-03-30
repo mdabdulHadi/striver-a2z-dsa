7573f97b2ff610e763aaa1cce39bb87a2f313289
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-03-30 21:20:10 +0530

    Add files via upload

---
+++ p6.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Print(int n)
+    {
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 0; j <=n-i+1; j++)
+            {
+                Console.Write(j+ " ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print(n);
+        }
+    }
+}
