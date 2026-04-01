890babb0042f0b7e2e34f19a9569dfd3c9bd220f
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-04-01 22:05:51 +0530

    Add files via upload

---
+++ p9.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Print1(int n)
+    {
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 0; j <n-i-1; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j <2*i-1; j++)
+            {
+                Console.Write("*");
+            }
+            for (int j = 0; j <n-i-1; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static void Print2(int n)
+    {
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 0; j <i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j <2*n-(2*i+1); j++)
+            {
+                Console.Write("*");
+            }
+            for (int j = 0; j <i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print1(n);
+            Print2(n);
+        }
+    }
+}
