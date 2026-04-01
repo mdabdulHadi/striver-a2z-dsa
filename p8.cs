4f5bbdef7f6d93c6ead36d047e61e35ed45108f9
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-04-01 22:04:26 +0530

    Add files via upload

---
+++ p8.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Print(int n)
+    {
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 0; j <i; j++)
+            {
+                Console.Write(" ");
+            }
+            for (int j = 0; j <2*n-(2*i+1); j++)
+            {
+                Console.Write("*");
+            }
+            for (int j = 0; j <i; j++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print(n);
+        }
+    }
+}
