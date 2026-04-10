b4b8db010c9783831cd0c475720bb5550e9fb220
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-04-10 08:26:31 +0530

    Add files via upload

---
+++ p19.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+class Program
+{
+    static void Print(int n)
+    {
+        int initialSpace=0;
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 1; i <=n-i-1; j++)
+            {
+                Console.Write("*");
+            }
+            for(int j=0; j<initialSpace;j++){
+                Console.Write(" ");
+
+            }
+            for (int j = 1; i <=n-i-1; j++)
+            {
+                Console.Write("*");
+            }
+            initialSpace+=2;
+            Console.WriteLine();
+        }
+        int initialSpace=8;
+        for (int i = 1; i <=n; i++)
+        {
+            for (int j = 1; i <=n-i-1; j++)
+            {
+                Console.Write("*");
+            }
+            for(int j=0; j<initialSpace;j++){
+                Console.Write(" ");
+
+            }
+            for (int j = 1; i <=n-i-1; j++)
+            {
+                Console.Write("*");
+            }
+            initialSpace-=2;
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print(n);
+        }
+    }
+}
