fc666e07dea9c8fc80fbcf2ab23d128a8d79a4ca
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-04-10 08:16:07 +0530

    Add files via upload

---
+++ p18.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Print(int n)
+    {
+        for (int i = 0; i <n; i++)
+        {
+            for (char ch='E'-i;; ch <='E'; ch++)
+            {
+                Console.Write(ch+" ");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print(n);
+        }
+    }
+}  
