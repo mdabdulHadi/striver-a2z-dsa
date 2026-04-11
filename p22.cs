b9703b7c27cc157303bb397b83b9652a559db9e0
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-04-11 15:45:32 +0530

    Add files via upload

---
+++ p22.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Print(int n)
+    {
+        for (int i = 0; i <2*n-1; i++)
+        {
+            for (int j = 0; j <2*n-1; j++)
+            {
+                int top=i;
+                int left=j;
+                int right=(2*n-2)-j;
+                int down=(2*n-2)-i;
+                Console.Write(n-min(min(top, down), min(left, right)));  
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print(n);
+        }
+    }
+}
