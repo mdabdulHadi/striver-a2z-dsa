31ca6d06decbe7de903beef57d3838b51f02d8e7
Mohammad Abdul Hadi <45118566+mdabdulHadi@users.noreply.github.com>
2026-04-11 15:34:24 +0530

    Add files via upload

---
+++ p20.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Print(int n)
+    {
+        int spaces=2*n-2;
+        for (int i = 1; i <=2*n-1; i++)
+        {
+            int stars=i;
+            if(i>n) stars=2*n-i;
+            for (int j = 1; j <=stars; j++)
+            {
+                Console.Write("*");
+            }
+            for(int j=1; j<=spaces;j++){
+                Console.Write(" ");
+            }
+            for (int j = 1; i <stars; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            if(i<n) spaces-=2;
+            else spaces+=2
+        }
+    }
+
+    static void Main()
+    {
+        int t = int.Parse(Console.ReadLine());
+        
+        for (int i = 0; i < t; i++)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Print(n);
+        }
+    }
+}
